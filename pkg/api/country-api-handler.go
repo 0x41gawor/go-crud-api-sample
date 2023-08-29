0c54ad9bca82d847d31eaa2fb76371d8d861fc6d
0x41gawor <41gawor@gmail.com>
2023-08-29 19:40:14 +0000

    feat(api): implement create endpoint

--- pkg/api/country-api-handler.go
+++ pkg/api/country-api-handler.go
@@ -1,8 +1,11 @@
 package api
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 
+	"github.com/0x41gawor/go-crud-api-sample/pkg/model"
 	"github.com/0x41gawor/go-crud-api-sample/pkg/repo"
 )
 
@@ -20,16 +23,33 @@ func NewCountryApiHandler(repo repo.CountryRepository) *CountryApiHandler {
 func (this *CountryApiHandler) handleCountry(w http.ResponseWriter, r *http.Request) error {
 	switch r.Method {
 	case "GET":
-		return this.list(w, r)
+		return this.List(w, r)
+	case "POST":
+		return this.Create(w, r)
 	default:
 		return WriteJSON(w, http.StatusOK, "error: method not allowed")
 	}
 }
 
-func (this *CountryApiHandler) list(w http.ResponseWriter, r *http.Request) error {
+func (this *CountryApiHandler) List(w http.ResponseWriter, r *http.Request) error {
 	res, err := this.repo.List()
 	if err != nil {
 		return err
 	}
 	return WriteJSON(w, http.StatusOK, res)
+}
+
+func (this *CountryApiHandler) Create(w http.ResponseWriter, r *http.Request) error {
+	model := new(model.Country)
+
+	err := json.NewDecoder(r.Body).Decode(model)
+	if err != nil {
+		return WriteJSON(w, http.StatusOK, fmt.Sprintf("error: %s", err.Error()))
+	}
+	id, err := this.repo.Create(model)
+	if err != nil {
+		return WriteJSON(w, http.StatusOK, fmt.Sprintf("error: %s", err.Error()))
+	}
+
+	return WriteJSON(w, http.StatusOK, fmt.Sprintf("createdId: %d", id))
 }
