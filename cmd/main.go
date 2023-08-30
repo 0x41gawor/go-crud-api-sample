ab4436f5733093065b498af11092e93c7822bf0c
0x41gawor <41gawor@gmail.com>
2023-08-30 13:47:34 +0000

    feat(repo): implement user repository

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
-	"github.com/0x41gawor/go-crud-api-sample/pkg/api"
+	"fmt"
+
+	"github.com/0x41gawor/go-crud-api-sample/pkg/model"
+	"github.com/0x41gawor/go-crud-api-sample/pkg/repo"
 )
 
 func main() {
-	server := api.NewServer(":3000")
-	server.Run()
+	// server := api.NewServer(":3000")
+	// server.Run()
+	user := model.NewUser(0, "admin", "admin")
+	fmt.Println(user)
+	db, _ := repo.NewDatabaseConnectionHelper()
+	repo := repo.NewUserRepository(db.DB)
+	id, _ := repo.Create(user)
+	println(id)
+	user, err := repo.FindByLogin("admin")
+	fmt.Println(user, err)
 }
---
+++ pkg/repo/user-repository.go
@@ -0,0 +1,71 @@
+package repo
+
+import (
+	"database/sql"
+	"errors"
+	"fmt"
+
+	"github.com/0x41gawor/go-crud-api-sample/pkg/model"
+)
+
+type UserRepository struct {
+	db *sql.DB
+}
+
+func NewUserRepository(db *sql.DB) *UserRepository {
+	return &UserRepository{
+		db: db,
+	}
+}
+
+func (this *UserRepository) Create(m *model.User) (int64, error) {
+	query := fmt.Sprintf(
+		"INSERT INTO users(login, password) VALUES ('%s', '%s');",
+		m.Login,
+		m.Password,
+	)
+
+	res, err := this.db.Exec(query)
+
+	if err != nil {
+		return 0, err
+	}
+
+	lastId, err := res.LastInsertId()
+	if err != nil {
+		return 0, err
+	}
+
+	return lastId, nil
+}
+
+func (this *UserRepository) FindByLogin(login string) (*model.User, error) {
+	query := fmt.Sprintf(
+		"SELECT * FROM users WHERE login = '%s'",
+		login,
+	)
+
+	res, err := this.db.Query(query)
+	defer res.Close()
+
+	if err != nil {
+		return nil, err
+	}
+
+	model := new(model.User)
+
+	if res.Next() {
+		err := res.Scan(
+			&model.Id,
+			&model.Login,
+			&model.Password,
+		)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		return nil, errors.New("No item with given id")
+	}
+
+	return model, nil
+}
